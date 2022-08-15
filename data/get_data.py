53b512daa996f2fd4cc35e5b9615a7e9ddeb84f7
Oscar <osw282@gmail.com>
2022-08-15 16:08:21 +0100

    Added script to download data and model training

---
+++ data/get_data.py
@@ -0,0 +1,57 @@
+# A script to download and process the training data
+
+import logging
+import os
+import zipfile
+import numpy as np
+import pandas as pd
+
+# os.environ['KAGGLE_USERNAME'] = "<your-kaggle-username>"
+# os.environ['KAGGLE_KEY'] = "<your-kaggle-api-key>"
+os.environ['KAGGLE_USERNAME'] = "oscarw282"
+os.environ['KAGGLE_KEY'] = "fe5c2f51e24a2ac7b25d39405667d0eb"
+
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+def setup_logger():
+    logging.basicConfig(
+        level=logging.INFO, 
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.StreamHandler()
+        ]
+    )
+
+def authenticate_api():
+    api = KaggleApi()
+    logging.info("Authenticating API keys")
+    api.authenticate()
+    logging.info("Keys authenticated")
+    return api
+
+def download_dataset(api):
+    logging.info("Downloading house pricing data from Kaggle")
+    api.dataset_download_files('harlfoxem/housesalesprediction', path="data")
+
+def prepare_dataset():
+    logging.info("Extracting dataset zip file")
+    with zipfile.ZipFile('data\housesalesprediction.zip', 'r') as zip_ref:
+        zip_ref.extractall('data')
+
+    logging.info("Processing data")
+
+    house_data = pd.read_csv("data\kc_house_data.csv")
+
+    house_data['date'] = pd.to_datetime(house_data['date'])
+
+    house_data.set_index('date', inplace=True)
+    house_data.to_csv('data\processed_house_data.csv')
+
+    house_data.head()
+    logging.info("processed_house_data.csv generated")
+
+if __name__ == "__main__":
+    setup_logger()
+    api = authenticate_api()
+    download_dataset(api)
+    prepare_dataset()
---
+++ pipeline/train.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import numpy as np
+import logging
+import pickle
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+def setup_logger():
+    logging.basicConfig(
+        level=logging.INFO, 
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.StreamHandler()
+        ]
+    )
+
+def prepare_data():
+    logging.info("Preparing data for train and test")
+    df = pd.read_csv('data\processed_house_data.csv', index_col='date')
+
+    target = 'price'
+    prediction = 'predicted_price'
+    datetime = 'date'
+
+    features = ['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors',
+        'waterfront', 'view', 'condition', 'grade', 'yr_built']
+
+    X = df[features].values
+    y = df[target].values
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=28)
+
+    return X_train, X_test, y_train, y_test
+
+def model_setup():
+    logging.info("Creating Random Forest Regressor model")
+    model = RandomForestRegressor(random_state=28, verbose=1)
+    return model
+
+def train(model):
+    logging.info("Training model")
+    model.fit(X_train, y_train)
+    logging.info("Training Completed")
+
+def evaluate(model):
+    logging.info("Evaluating model on test set")
+    predictions = model.predict(X_test)
+    mse = mean_squared_error(y_test, predictions)
+    mae = mean_absolute_error(y_test, predictions)
+    rmse = mean_squared_error(y_test, predictions, squared=False)
+    r2 = r2_score(y_test, predictions)
+
+    print(f"Mean Squared Error: {mse}")
+    print(f"Mean Absolute Error: {mae}")
+    print(f"Root Mean Squared Error: {rmse}")
+    print(f"R-Squared: {r2}")
+
+def save_model(model):
+    with open('pipeline\model.pkl','wb') as f:
+        pickle.dump(model, f)
+
+if __name__ == "__main__":
+    setup_logger()
+    X_train, X_test, y_train, y_test = prepare_data()
+    model = model_setup()
+    train(model)
+    evaluate(model)
+    save_model(model)
